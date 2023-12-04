ed63e264c341a408154431612532d3589e4e7d76
danieledisco <daniele.disco.1965@gmail.com>
2023-12-04 01:08:31 +0100

    Jsnack4

--- jsnack4/jscipt.js
+++ jsnack4/jscipt.js
@@ -13,9 +13,62 @@ Tools
  - string.localeCompare(string) = 0 if the string are equal
 
 PseudoCode
-
+ - inizializza un array con i nomi degli invitati
+ - inizializza la variabile go a false
+ - chiedi il nome
+ - confronta carattere per carattere i nomi degli autorizzati con il tuo
+    - se già la lumghezza è diversa salta
+    - se la corrispondenza arriva a fine cambia go in true ed interromi il ciclo
+ - Sulla base di autorizzato dai il benvenuto o saluta
 
 
 
 
 */
+
+//  -inizializza un array con i nomi degli invitati
+const authorized = ['Carlo', 'Federico', 'Nicola', 'Giovanni', 'Marco'];
+let go=false;
+
+// - chiedi il nome
+const nome = prompt("Introduci il tuo nome");
+
+// - confronta carattere per carattere i nomi degli autorizzati con il tuo
+//    - se già la lumghezza è diversa salta
+//    - se la corrispondenza arriva a fine cambia go in true ed interromi il ciclo
+for ( i=0; i<authorized.length; i++)
+{
+    if (go)
+        break;
+
+    if (authorized[i].length != nome.length)
+    {
+        go = false;
+        continue;
+    }
+    else
+    {
+        for ( j=0; j<nome.length; j++)
+        {
+            if(authorized[i][j] != nome[j])
+            {
+                go = false;
+                break;
+            }
+            else
+            {
+                go = true;
+            }
+        }
+    }
+}
+
+// - Sulla base di autorizzato dai il benvenuto o saluta
+if (go)
+{
+    confirm("Benvenuto alla festa " + nome);
+}
+else
+{
+    confirm("Mi dispiace " + nome + " non sei tra gli invitati")
+}
